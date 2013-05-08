5f4fd979e3d8c9b19befa1f7c3c75fd8a32224fa
tugberkugurlu <tugberkugurlu@gmail.com>
2013-05-08 22:52:53 +0300

    initial dummy but pure owin sample (taken from http://weblogs.asp.net/pglavich/archive/2013/04/05/owin-katana-and-getting-started.aspx)

---
+++ RedirectoR/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedirectoR {
+
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public partial class Startup {
+
+        public void Configuration(IAppBuilder app)  {
+
+            app.Use(new Func<AppFunc, AppFunc>(ignoreNextApp => (AppFunc)Invoke));
+        }
+
+        public Task Invoke(IDictionary<string, object> environment) 
+        {
+            byte[] responseBytes = ASCIIEncoding.UTF8.GetBytes(
+                string.Format("Serviced request on {0} at {1}", DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString()));
+
+            Stream responseStream = (Stream)environment["owin.ResponseBody"];
+            IDictionary<string, string[]> responseHeaders = (IDictionary<string, string[]>)environment["owin.ResponseHeaders"];
+            responseHeaders["Content-Length"] = new[] { responseBytes.Length.ToString(CultureInfo.InvariantCulture) };
+            responseHeaders["Content-Type"] = new[] { "text/plain" };
+            return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+        }
+    }
+}
